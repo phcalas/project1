f86aefdedc3897f573719f5699eb4a97bf04a3c1
Jenkins <jenkins@eva>
2014-12-13 18:31:04 +0100

    Launch installtion by hand for other repositorieswq

--- openSuse_13.2/install_update.sh
+++ openSuse_13.2/install_update.sh
@@ -5,7 +5,9 @@ zypper addrepo  http://pkg.jenkins-ci.org/opensuse-stable/ jenkins
 # Refresh all repos 
 zypper -n refresh
 # Add some packages directly
-zypper -n in docker thunderbird go chrome jenkins java-1_8_0-openjdk-devel java-1_7_0-openjdk-devel gparted virtualbox
+zypper -n in docker thunderbird go java-1_8_0-openjdk-devel java-1_7_0-openjdk-devel gparted virtualbox
 # Finally Update whole system
 zypper -n up -l
+# Install other packages by hand
+echo "Lanch: zypper  in google-chrome jenkins" 
 
