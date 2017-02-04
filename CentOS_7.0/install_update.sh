ca618dfbb8a7c90cb8f05b6b5426536cfa87d201
Phil <phcalas@example.com>
2017-02-04 16:32:19 +0100

    New fail2ban version

--- CentOS_7.0/install_update.sh
+++ CentOS_7.0/install_update.sh
@@ -1,7 +1,11 @@
 # Add Fail2ban repo
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum -y install gamin-python python-inotify whois
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-0.9.5-3.el7.noarch.rpm \
+	https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-firewalld-0.9.5-3.el7.noarch.rpm \
+	https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-sendmail-0.9.5-3.el7.noarch.rpm \
+	https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-server-0.9.5-3.el7.noarch.rpm
 # Add some packages directly
-yum install -y docker gparted git fail2ban vim nmap
+yum install -y docker gparted vim nmap
 # Prepare Fail2ban configuration (add local IP for avoiding ban)
 sed -e 's/^\(ignoreip .*\)$/\1 192.168.0.0\/24/g' /etc/fail2ban/jail.conf > /etc/fail2ban/jail.local
 systemctl restart fail2ban.service
