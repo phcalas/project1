4b9ed4f77915b1f5718024f5ac452bdcbba29e9f
Ph Calas <phcalas@free.fr>
2019-06-22 23:40:22 +0200

    Now fail2ban is integrated into repo

--- CentOS_7.0/install_update.sh
+++ CentOS_7.0/install_update.sh
@@ -1,9 +1,5 @@
 # Add Fail2ban repo
-yum -y install gamin-python python-inotify whois
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-0.9.5-3.el7.noarch.rpm \
-	https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-firewalld-0.9.5-3.el7.noarch.rpm \
-	https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-sendmail-0.9.5-3.el7.noarch.rpm \
-	https://dl.fedoraproject.org/pub/epel/7/x86_64/f/fail2ban-server-0.9.5-3.el7.noarch.rpm
+yum -y install gamin-python python-inotify whois fail2ban
 # Add some packages directly
 yum install -y docker gparted vim nmap
 # Prepare Fail2ban configuration (add local IP for avoiding ban)
