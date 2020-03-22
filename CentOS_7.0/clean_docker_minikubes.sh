5115e21ea3c15311ce09200764ea85c3a93413dc
Ph Calas <phcalas@free.fr>
2020-03-22 12:36:42 +0100

    Initial procedure to remove entirely Docker & Kubernetes & Minikube

---
+++ CentOS_7.0/clean_docker_minikubes.sh
@@ -0,0 +1,17 @@
+# Stop minikbe & docker
+minikube stop
+systemctl stop docker
+systemctl disable docker
+systemctl disable kubernates
+# Remove installation & images & sockets
+yum remove docker-ce docker-ce-client kubectl kubeadm kubelet
+rm -rf /var/lib/docker* /var/run/docker*
+# Remove repos
+rm -f /etc/yum.repos.d/kubernetes.repo /etc/yum.repos.d/docker-ce.repo
+# Remove minikube
+rm /usr/local/bin/minikube
+# Remove firewall rules
+rm /etc/firewalld/services/kubernetes.xml
+firewall-cmd --reload
+# Remove Home context
+rm -rf $HOME/.kube $HOME/.minikube
