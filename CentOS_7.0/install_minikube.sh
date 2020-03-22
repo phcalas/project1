c9df0b0ea5fbc5fe3b10f4b33e235aa6e1dddb8a
Ph Calas <phcalas@free.fr>
2020-03-22 11:25:57 +0100

    Fast & dirty Minikube installation

---
+++ CentOS_7.0/install_minikube.sh
@@ -0,0 +1,61 @@
+# Minikube installation on CentOS
+
+# prerequisite
+
+# Add repo: Docker, Kubernetes
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Remove SWAP on CentOS (mandatory for kubernetes)
+swapoff -a
+#to comment Swap mount at each reboot
+sed -i '/swap/s/^/#/' /etc/fstab
+
+yum install -y qemu-kvm qemu-guest-agent
+modprobe kvm # Verif with #lsmod | grep kvm
+modprobe virtio # Verifi with #lsmod | grep virtio
+yum install -y docker-ce docker-ce-client kubectl kubeadm kubelet
+yum versionlock docker-ce docker-ce-client kubectl kubeadm kubelet
+systemctl start docker
+#Open port for
+# preferable to define a service
+# firewall-cmd --zone=public --permanent --add-port=8443/tcp
+# firewall-cmd --zone=public --permanent --add-port=10250/tcp
+cat > /etc/firewalld/services/kubernetes.xml << EOF
+<?xml version="1.0" encoding="utf-8"?>
+<service>
+  <short>Kubernetes minimun</short>
+  <description>This is just an example service.  It probably shouldn't be used on a real system.</description>
+  <port protocol="tcp" port="8443"/>
+  <port protocol="tcp" port="10250"/>
+</service>
+EOF
+firewall-cmd --reload #Take into account this new configuration for Kubernetes
+firewall-cmd --get-services | grep kubernetes
+
+# Launch install of the minikube instance
+systemctl enable kubelet.service
+minikube start --driver=kvm2
+
+# Verification
+diff /etc/kubernetes/kubelet.conf $HOME/.kube/config
+kubectl get nodes
+kubectl get namespace
+kubectl create deployment hello-node --image=gcr.io/hello-minikube-zero-install/hello-node
+kubectl get deployments
+kubectl expose deployment hello-node --type=LoadBalancer --port=8080
+kubectl get services
+minikube service hello-node # get the conf of exposed loadbalancer
+
+
+
--- CentOS_7.0/install_update.sh
+++ CentOS_7.0/install_update.sh
@@ -1,7 +1,7 @@
 # Add Fail2ban repo
 yum -y install gamin-python python-inotify whois fail2ban
 # Add some packages directly
-yum install -y docker gparted vim nmap
+yum install -y docker-cd docker-ce-client gparted vim nmap
 # Prepare Fail2ban configuration (add local IP for avoiding ban)
 sed -e 's/^\(ignoreip .*\)$/\1 192.168.0.0\/24/g' /etc/fail2ban/jail.conf > /etc/fail2ban/jail.local
 systemctl restart fail2ban.service
